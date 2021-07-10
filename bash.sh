b7a9786ac33419f68ac05e3cc3e5a9288e913e49
TheMegaNear <fluke34261@users.noreply.github.com>
2021-07-10 15:00:01 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,8 +2,26 @@
 
 brew install python@3.9
 brew install node@14
-brew install zsh
+
 brew install cask visual-studio-code
 brew install cask tg-pro
 brew install cask tableplus
 brew install cask iterm2
+
+# setup oh my zsh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i'' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+
+# install powerline font
+# https://gist.github.com/DukeNgn/9312a65e1535b3b1bffd9fed2c892079
+cd ~
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+# install solarized
+cd ~
+git clone git://github.com/altercation/solarized.git
+cd ~/solarized/iterm2-colors-solarized
+open .

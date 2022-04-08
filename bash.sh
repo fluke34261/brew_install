67d7820663ff3786dc129ad27de478b4a9abbaa8
TheMegaNear <fluke34261@users.noreply.github.com>
2022-04-08 22:01:13 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -19,20 +19,20 @@ brew install python@3.9
 brew install openjdk
 brew install --cask intellij-idea-ce
 
-# setup oh my zsh
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i'' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+# #setup oh my zsh
+# brew install zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sed -i'' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 
-# install powerline font
-# https://gist.github.com/DukeNgn/9312a65e1535b3b1bffd9fed2c892079
-cd ~
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
+# #install powerline font
+# #https://gist.github.com/DukeNgn/9312a65e1535b3b1bffd9fed2c892079
+# cd ~
+# git clone https://github.com/powerline/fonts.git
+# cd fonts
+# ./install.sh
 
-# install solarized
-cd ~
-git clone git://github.com/altercation/solarized.git
-cd ~/solarized/iterm2-colors-solarized
-open .
+# #install solarized
+# cd ~
+# git clone git://github.com/altercation/solarized.git
+# cd ~/solarized/iterm2-colors-solarized
+# open .

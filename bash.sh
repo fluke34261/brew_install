067a2df2f36906871ec801630929b502eb9a27b3
TheMegaNear <fluke34261@users.noreply.github.com>
2022-04-08 21:56:25 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install cask visual-studio-code
 brew install cask tg-pro

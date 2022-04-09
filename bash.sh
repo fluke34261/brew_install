53f7b71b4ac2ca09c9962fdb69fae2b0dda19d45
TheMegaNear <fluke34261@users.noreply.github.com>
2022-04-09 10:37:49 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -16,23 +16,6 @@ brew install python@3.9
 # echo 'export PATH="/usr/local/opt/node@14/bin:$PATH"' >> ~/.zshrc
 # brew postinstall node@14
 # npm install --global yarn
-brew install openjdk
+# brew install openjdk
+brew install --cask oracle-jdk
 brew install --cask intellij-idea-ce
-
-# #setup oh my zsh
-# brew install zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# sed -i'' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
-
-# #install powerline font
-# #https://gist.github.com/DukeNgn/9312a65e1535b3b1bffd9fed2c892079
-# cd ~
-# git clone https://github.com/powerline/fonts.git
-# cd fonts
-# ./install.sh
-
-# #install solarized
-# cd ~
-# git clone git://github.com/altercation/solarized.git
-# cd ~/solarized/iterm2-colors-solarized
-# open .
